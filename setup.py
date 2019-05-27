cd7ea627e9103c78e11e6d8823d2a8cfd0c07fbb
= <rafael.vettori@gmail.com>
2019-05-27 15:39:11 -0300

    publish version 0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='qreport',
-      version='0.5',
+      version='0.6',
       description='Simple Report',
       url='https://github.com/rvettori/qreport',
       author='Rafael Vettori',
